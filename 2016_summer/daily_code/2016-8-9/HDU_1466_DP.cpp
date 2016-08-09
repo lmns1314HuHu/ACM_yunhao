766e8f4387598ad81c1dabec803a268c0d8549aa
lmns1314HuHu <851549562@qq.com>
2016-08-09 21:33:22 +0800

    daily_commit

---
+++ 2016_summer/daily_code/2016-8-9/HDU_1466_DP.cpp
@@ -0,0 +1,32 @@
+/**==================================
+ | Author: YunHao
+ | OJ: 
+ | Kind: 
+ | Date: 
+ | Describe:
+ |          
+ |          
+ =================================**/
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+int dp[22][200];
+
+int main()
+{
+    dp[1][0] = 1;
+    dp[2][0] = 1;
+    dp[2][1] = 1;
+    for (int i = 3; i <= 21; i++){
+        
+    }
+    return 0;
+}
---
+++ 2016_summer/daily_code/2016-8-9/HDU_2197_DP.cpp
@@ -0,0 +1,29 @@
+/**==================================
+ | Author: YunHao
+ | OJ: 
+ | Kind: 
+ | Date: 
+ | Describe:
+ |          
+ |          
+ =================================**/
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    int n;
+    while(~scanf("%d", &n)){
+        int dp = 1;
+        for (int i = )
+    }
+    return 0;
+}
