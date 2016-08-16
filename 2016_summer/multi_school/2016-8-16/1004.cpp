c7619ff942ddb75264a4afe8b8dd6925e9195325
lmns1314HuHu <851549562@qq.com>
2016-08-16 14:33:35 +0800

    daily_commit

---
+++ 2016_summer/multi_school/2016-8-16/1004.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+int t;
+
+int main()
+{
+    
+    return 0;
+}
